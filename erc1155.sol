e9a1ab1030bcd34232d1d4ed676e5544e6eaed01
muhammadwaqarqsol <139227066+muhammadwaqarqsol@users.noreply.github.com>
2024-06-26 00:45:36 +0500

    Update erc1155.sol

--- erc1155.sol
+++ erc1155.sol
@@ -1,1 +1,35 @@
 
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract MyToken is ERC1155, Ownable {
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+
+    function setURI(uint256 tokenId, string memory newuri) public onlyOwner {
+        _tokenURIs[tokenId] = newuri;
+    }
+
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
